305aace70c9065a68cdf01cdb3dc0d005f35eddf
f18326186224 <f18326186224@gmail.com>
2023-12-11 02:31:57 +0800

    ✨ feat: 初始化数据库

--- src/libsql/db.py
+++ src/libsql/db.py
@@ -1,5 +1,60 @@
 # libsql数据库交互类
+import os
+from venv import logger
+import libsql_client
+from libsql_client import ResultSet, ClientSync
 from libsql import sql_scripts
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+# 数据库地址
+TURSO_DB_URL = os.environ['TURSO_DB_URL']
+# 数据库认证token
+TURSO_DB_AUTH_TOKEN = os.environ['TURSO_DB_AUTH_TOKEN']
+
+
+def get_client():
+    """获取数据库客户端
+
+    Returns:
+        [type]: [description]
+    """
+    return libsql_client.create_client_sync(
+        url=TURSO_DB_URL, auth_token=TURSO_DB_AUTH_TOKEN)
+
+
+def init_db(client: ClientSync):
+    """初始化数据库
+
+    Args:
+        client (ClientSync): 客户端对象
+    """
+    # 如果电影表不存在，则创建电影表
+    if len(client.execute(sql_scripts.TABLE_MOVIE_EXISTS).rows) == 0:
+        logger.info('电影表不存在,创建电影表...')
+        client.execute(sql_scripts.CREATE_TABLE_MOVIE)
+        # 创建索引
+        client.execute(sql_scripts.CREATE_INDEX_MOVIE)
+        logger.info('创建电影表成功!')
+    # 如果剧集表不存在，则创建剧集表
+    if len(client.execute(sql_scripts.TABLE_SHOW_EXISTS).rows) == 0:
+        logger.info('剧集表不存在,创建剧集表...')
+        client.execute(sql_scripts.CREATE_TABLE_SHOW)
+        # 创建索引
+        client.execute(sql_scripts.CREATE_INDEX_SHOW)
+        logger.info('创建剧集表成功!')
+    # 如果季(剧集)表不存在，则创建季(剧集)表
+    if len(client.execute(sql_scripts.TABLE_SEASON_EXISTS).rows) == 0:
+        logger.info('季(剧集)表不存在,创建季(剧集)表...')
+        client.execute(sql_scripts.CREATE_TABLE_SEASON)
+        logger.info('创建季(剧集)表成功!')
+
+
+client = get_client()
+init_db(client)
 
 
 def update_movies(watched_movies: list):
--- src/libsql/sql_scripts.py
+++ src/libsql/sql_scripts.py
@@ -1,8 +1,20 @@
 # 判断电影表是否存在
-TABLE_MOVIE_EXISTS = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='movie';"
+TABLE_MOVIE_EXISTS = "SELECT name FROM sqlite_master WHERE type='table' AND name='movie'"
 # 初始化电影表
-CREATE_TABLE_MOVIE = "CREATE TABLE movie ( id INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER NOT NULL, name TEXT NOT NULL, duration INTEGER NOT NULL, type TEXT NOT NULL, FOREIGN KEY (movie_id) REFERENCES movie (id) );"
+CREATE_TABLE_MOVIE = "CREATE TABLE movie ( movie_id TEXT PRIMARY KEY, movie_name TEXT, movie_description TEXT, release_year TEXT, cover_image_url TEXT, rating TEXT, share_link TEXT )"
+# 创建电影表索引
+CREATE_INDEX_MOVIE = "CREATE INDEX idx_movie_name ON movie (movie_name)"
+
+
 # 判断剧集表是否存在
-TABLE_SHOW_EXISTS = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='show';"
+TABLE_SHOW_EXISTS = "SELECT name FROM sqlite_master WHERE type='table' AND name='show'"
 # 创建剧集表
-CREATE_TABLE_SHOW = "CREATE TABLE show ( id INTEGER PRIMARY KEY AUTOINCREMENT, show_id INTEGER NOT NULL, start_time TEXT NOT NULL, FOREIGN KEY (movie_id) REFERENCES movie (id) );"
+CREATE_TABLE_SHOW = "CREATE TABLE  show ( show_id TEXT PRIMARY KEY, show_name TEXT, show_description TEXT, release_year TEXT, cover_image_url TEXT, rating TEXT, share_link TEXT )"
+# 创建剧集表索引
+CREATE_INDEX_SHOW = "CREATE INDEX  idx_show_name ON show (show_name)"
+
+
+# 判断季(剧集)表是否存在
+TABLE_SEASON_EXISTS = "SELECT name FROM sqlite_master WHERE type='table' AND name='season'"
+#  创建季(剧集)表
+CREATE_TABLE_SEASON = "CREATE TABLE  season ( season_id TEXT PRIMARY KEY, show_id TEXT, season_number TEXT, season_description TEXT, release_year TEXT, rating TEXT, FOREIGN KEY (show_id) REFERENCES show (show_id) )"
