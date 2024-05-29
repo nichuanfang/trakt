2585b3a600646156150fa751cdefa468e70709d6
f18326186224 <f18326186224@gmail.com>
2024-05-29 19:05:53 +0800

    同步剧集数据采集功能开发完成

--- src/core/tmdb.py
+++ src/core/tmdb.py
@@ -1,10 +1,12 @@
 import os
-from utils import area_util
-from trakt.movies import Movie
-from trakt.tv import TVShow, TVSeason, TVEpisode
+
+from tmdbv3api import Movie as TMDB_Movie
 from tmdbv3api import TMDb
-from tmdbv3api import Movie
-from tmdbv3api import TV
+from tmdbv3api import TV as TMDB_TV
+from trakt.movies import Movie
+from trakt.tv import TVShow
+
+from utils import area_util
 
 # 初始化tmdb
 tmdb = TMDb()
@@ -13,22 +15,54 @@ tmdb.api_key = os.environ.get('TMDB_API_KEY')
 # 设置tmdb的语言为中文
 tmdb.language = 'zh-CN'
 #  初始化电影和剧集对象
-tmdb_movie = Movie()
-tmdb_tv = TV()
+tmdb_movie = TMDB_Movie()
+tmdb_tv = TMDB_TV()
 
 
-def convert2zh(movie: Movie):
-    details = tmdb_movie.details(movie_id=movie.tmdb)
-    # 标题
-    movie.title = details.title
-    # 剧情简介
-    movie.overview = details.overview
-    # 评分
-    movie.rating = details.vote_average
-    # 封面图片
-    movie.poster = details.poster_path
-    # 获取iso_3166_1国家地区代码
-    country_code = details.production_countries[0]['iso_3166_1']
-    # 根据iso_3166_1国家代码获取国家名称
-    country_name = area_util.get_country_name_by_code(country_code)
-    return country_name
+def convert2zh_movie(movie: Movie):
+	"""
+	将电影的元信息转为中文
+	@param movie:  电影对象
+	@return: 处理好的国家名称
+	"""
+	details = tmdb_movie.details(movie_id=movie.tmdb)
+	# 标题
+	movie.title = details.title
+	# 剧情简介
+	movie.overview = details.overview
+	# 评分
+	movie.rating = details.vote_average
+	# 封面图片
+	movie.poster = details.poster_path
+	# 获取iso_3166_1国家地区代码
+	country_code = details.production_countries[0]['iso_3166_1']
+	# 根据iso_3166_1国家代码获取国家名称
+	country_name = area_util.get_country_name_by_code(country_code)
+	return country_name
+
+
+def convert2zh_show(show: TVShow):
+	"""
+	将剧集的元信息转为中文
+	@param show:  剧集对象
+	@return: 处理好的国家名称
+	"""
+	details = tmdb_tv.details(tv_id=show.tmdb)
+	# 标题
+	show.title = details.name
+	# 剧情简介
+	show.overview = details.overview
+	# 评分
+	show.rating = details.vote_average
+	# 封面图片
+	show.poster = details.poster_path
+	# 获取iso_3166_1国家地区代码
+	country_code = details.production_countries[0]['iso_3166_1']
+	# 根据iso_3166_1国家代码获取国家名称
+	country_name = area_util.get_country_name_by_code(country_code)
+	is_ended = 0
+	if details.status == 'Ended':
+		is_ended = 1
+	# 总季数
+	seasons_from_tmdb = details.seasons
+	return (country_name, is_ended, seasons_from_tmdb)
--- src/utils/base64_util.py
+++ src/utils/base64_util.py
@@ -1,23 +1,43 @@
 import base64
 import json
+
 from trakt.movies import Movie
-from trakt.tv import TVShow, TVSeason, TVEpisode
+from trakt.tv import TVShow
 
 
-def index(watched_movies: list[Movie] = None, watched_shows: list[TVShow] = None):
-    """生成电影或剧集的base64形式的索引
+def index_movies(watched_movies: list[Movie] = None):
+	"""生成电影base64形式的索引
 
-    Returns:
-        [type]: 索引
-    """
-    movie_data = []
-    for movie in watched_movies:
-        movie_data.append(
-            {
-                'title': movie.title,
-                'content': movie.overview,
-                'url': f'/culture/movies/detail/?tmdb_id={movie.tmdb}'
-            }
-        )
-    # 将movie_data转为json字符串  然后转为base64
-    return base64.b64encode(json.dumps(movie_data).encode('utf-8')).decode('utf-8')
+	Returns:
+		[type]: 索引
+	"""
+	movie_data = []
+	for movie in watched_movies:
+		movie_data.append(
+			{
+				'title': movie.title,
+				'content': movie.overview,
+				'url': f'/culture/movies/detail/?tmdb_id={movie.tmdb}'
+			}
+		)
+	# 将movie_data转为json字符串  然后转为base64
+	return base64.b64encode(json.dumps(movie_data).encode('utf-8')).decode('utf-8')
+
+
+def index_shows(watched_shows: list[TVShow] = None):
+	"""生成剧集的base64形式的索引
+
+	Returns:
+		[type]: 索引
+	"""
+	show_data = []
+	for show in watched_shows:
+		show_data.append(
+			{
+				'title': show.title,
+				'content': show.overview,
+				'url': f'/culture/movies/detail/?tmdb_id={show.tmdb}'
+			}
+		)
+	# 将movie_data转为json字符串  然后转为base64
+	return base64.b64encode(json.dumps(show_data).encode('utf-8')).decode('utf-8')
